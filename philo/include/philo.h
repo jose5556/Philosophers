fc6e49bdf92e1284aa3c02109bff481174661edc
jose5556 <josefilmeneses@gmail.com>
2025-01-23 08:59:48 +0000

    okay

---
+++ philo/include/philo.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/17 12:47:38 by joseoliv          #+#    #+#             */
+/*   Updated: 2024/12/17 13:42:52 by joseoliv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FHILO_H
+#define FILOS_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <pthread.h>
+
+#endif
---
+++ philo/main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/17 13:28:43 by joseoliv          #+#    #+#             */
+/*   Updated: 2024/12/17 13:33:15 by joseoliv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "./include/philo.h"
+
+int	main(int argc, char *argv[])
+{
+	if (argc != 5 || argc != 6)
+	{
+		printf("argument/s is/are not acceptable");
+		return (1);
+	}
+	
+}
