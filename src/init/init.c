4d7093eb956786662cb2e0b82585cd719acc7acd
jose5556 <josefilmeneses@gmail.com>
2025-02-25 07:01:41 +0000

    refactor names

--- src/init/init.c
+++ src/init/init.c
@@ -3,70 +3,80 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
+/*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 19:40:03 by joseoliv          #+#    #+#             */
-/*   Updated: 2025/02/20 11:40:38 by cereais          ###   ########.fr       */
+/*   Updated: 2025/02/25 06:33:39 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../../include/philo.h"
 
-t_table	init_table(char **argv, int argc)
+t_table	init_table(int argc, char **argv)
 {
 	t_table	table;
 
-	table.num_of_philos = ft_atoi(argv[0]);
-	table.time_to_die = (time_t)ft_atoi(argv[1]);
-	table.time_to_eat = (time_t)ft_atoi(argv[2]);
-	table.time_to_sleep = (time_t)ft_atoi(argv[3]);
-	if (argc == 6)  //TODO IF ARGV[4] == 0 -> NEEDS TO EXIT
-		table.num_times_philo_need_to_eat = ft_atoi(argv[4]);
+	table.numbers_of_philosophers = ft_atoi(argv[1]);
+	table.time_to_die = ft_atoi(argv[2]);
+	table.time_to_eat = ft_atoi(argv[3]);
+	table.time_to_sleep = ft_atoi(argv[4]);
+	table.finish = false;
+	table.done = false;
+	pthread_mutex_init(&table.info, NULL);
+	if (argc == 6)
+	{
+		table.number_of_times_to_eat = ft_atoi(argv[5]);
+		if (table.number_of_times_to_eat == 0)
+			exit(EXIT_SUCCESS);
+	}
 	else
-		table.num_times_philo_need_to_eat = -1;
+		table.number_of_times_to_eat = -1;
 	return (table);
 }
 
-t_philo	*init_philo(t_table table)
+t_philo	*init_philos(t_table *table)
 {
-	t_philo *philo;
-	int		i;
+	t_philo	*philos;
+	int		n;
 
-	i = 0;
-	philo = malloc(sizeof(t_philo) * table.num_of_philos);
-	if (!philo)
+	n = 0;
+	philos = (t_philo *)malloc(sizeof(t_philo) * table->numbers_of_philosophers);
+	if (!philos)
 		return (NULL);
-	while (i < table.num_of_philos)
+	while (n < table->numbers_of_philosophers)
 	{
-		philo[i].id = i + 1;
-		philo[i].nums_of_meals = 0;
-		philo[i].status = ALIVE;
-		philo[i].start_time = get_time();
-		philo[i].last_meal = get_time();
-		pthread_mutex_init(&philo[i].left_fork, NULL);
-		if (i == table.num_of_philos - 1)
-			philo[i].right_fork = &philo[0].left_fork;
+		philos[n].id = n + 1;
+		philos[n].table = table;
+		philos[n].number_of_meal = 0;
+		philos[n].status = ALIVE;
+		philos[n].last_meal = get_time();
+		philos[n].start_time = get_time();
+		pthread_mutex_init(&philos[n].left_fork, NULL);
+		pthread_mutex_init(&philos[n].philo, NULL);
+		if (n == table->numbers_of_philosophers - 1)
+			philos[n].right_fork = &philos[0].left_fork;
 		else
-			philo[i].right_fork = &philo[i + 1].left_fork;
-		philo[i].table = &table;
-		i++;
+			philos[n].right_fork = &philos[n + 1].left_fork;
+		n++;
 	}
+	return (philos);
 }
 
-void	init_threads(t_philo *philo)
+int	init_threads(t_philo *philos)
 {
-	int	i;
+	int	n;
 
-	i = 0;
-	if (philo->table->num_of_philos == 1)
-		return (handle_one_philo(philo));
-	while (i < philo->table->num_of_philos)
+	n = 0;
+	if (philos->table->numbers_of_philosophers == 1)
+		return (handle_one_philo(philos));
+	while (n < philos->table->numbers_of_philosophers)
 	{
-		if (pthread_create(&philo[i].philo_thread, NULL, &simulation, &philo[i]) != 0)
-		{
-			printf("pthread_create error!\n");
-			return ;
-		}
-		i++;
+		if (pthread_create(&philos[n].ph, NULL, &rotine, &philos[n]) != 0)
+			return (2);
+		n++;
 	}
+	set_bool(&philos->table->info, &philos->table->done, true);
+	while (get_bool(&philos->table->info, &philos->table->finish) == false)
+		monitoring(philos);
+	return (0);
 }
