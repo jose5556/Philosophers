4aec715796d430541107116e77b32befbac6c33a
jose5556 <josefilmeneses@gmail.com>
2025-02-25 07:02:51 +0000

    refactor time func

--- src/utils/time.c
+++ src/utils/time.c
@@ -3,19 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   time.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
+/*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 05:53:14 by cereais           #+#    #+#             */
-/*   Updated: 2025/02/17 06:11:13 by cereais          ###   ########.fr       */
+/*   Updated: 2025/02/25 06:08:10 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/philo.h"
 
-time_t	get_time()
+long long	get_time(void)
 {
-	struct timeval	tv;
+	long long			current_time;
+	struct timeval		st;
 
-	gettimeofday(&tv, NULL);
-	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
+	gettimeofday(&st, NULL);
+	current_time = (long long)st.tv_sec * 1000 + (long long)st.tv_usec / 1000;
+	return (current_time);
+}
+
+int	ft_usleep(long long milliseconds)
+{
+	long long	start;
+
+	start = get_time();
+	while ((get_time() - start) < milliseconds)
+		usleep(500);
+	return (0);
 }
