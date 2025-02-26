67d84225c887b6b7f56fe1bffdbdd37b908f4e5f
jose5556 <josefilmeneses@gmail.com>
2025-02-26 11:51:04 +0000

    removed exit func

--- src/init/init.c
+++ src/init/init.c
@@ -6,11 +6,11 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 19:40:03 by joseoliv          #+#    #+#             */
-/*   Updated: 2025/02/25 06:33:39 by joseoliv         ###   ########.fr       */
+/*   Updated: 2025/02/26 11:42:54 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../../include/philo.h"
+#include "../../include/philo.h"
 
 t_table	init_table(int argc, char **argv)
 {
@@ -24,11 +24,7 @@ t_table	init_table(int argc, char **argv)
 	table.done = false;
 	pthread_mutex_init(&table.info, NULL);
 	if (argc == 6)
-	{
 		table.number_of_times_to_eat = ft_atoi(argv[5]);
-		if (table.number_of_times_to_eat == 0)
-			exit(EXIT_SUCCESS);
-	}
 	else
 		table.number_of_times_to_eat = -1;
 	return (table);
@@ -37,43 +33,44 @@ t_table	init_table(int argc, char **argv)
 t_philo	*init_philos(t_table *table)
 {
 	t_philo	*philos;
-	int		n;
+	int		i;
 
-	n = 0;
-	philos = (t_philo *)malloc(sizeof(t_philo) * table->numbers_of_philosophers);
+	i = 0;
+	philos = (t_philo *)malloc(sizeof(t_philo)
+			* table->numbers_of_philosophers);
 	if (!philos)
 		return (NULL);
-	while (n < table->numbers_of_philosophers)
+	while (i < table->numbers_of_philosophers)
 	{
-		philos[n].id = n + 1;
-		philos[n].table = table;
-		philos[n].number_of_meal = 0;
-		philos[n].status = ALIVE;
-		philos[n].last_meal = get_time();
-		philos[n].start_time = get_time();
-		pthread_mutex_init(&philos[n].left_fork, NULL);
-		pthread_mutex_init(&philos[n].philo, NULL);
-		if (n == table->numbers_of_philosophers - 1)
-			philos[n].right_fork = &philos[0].left_fork;
+		philos[i].id = i + 1;
+		philos[i].table = table;
+		philos[i].number_of_meal = 0;
+		philos[i].status = ALIVE;
+		philos[i].last_meal = get_time();
+		philos[i].start_time = get_time();
+		pthread_mutex_init(&philos[i].left_fork, NULL);
+		pthread_mutex_init(&philos[i].philo, NULL);
+		if (i == table->numbers_of_philosophers - 1)
+			philos[i].right_fork = &philos[0].left_fork;
 		else
-			philos[n].right_fork = &philos[n + 1].left_fork;
-		n++;
+			philos[i].right_fork = &philos[i + 1].left_fork;
+		i++;
 	}
 	return (philos);
 }
 
 int	init_threads(t_philo *philos)
 {
-	int	n;
+	int	i;
 
-	n = 0;
+	i = 0;
 	if (philos->table->numbers_of_philosophers == 1)
 		return (handle_one_philo(philos));
-	while (n < philos->table->numbers_of_philosophers)
+	while (i < philos->table->numbers_of_philosophers)
 	{
-		if (pthread_create(&philos[n].ph, NULL, &rotine, &philos[n]) != 0)
+		if (pthread_create(&philos[i].ph, NULL, &rotine, &philos[i]) != 0)
 			return (2);
-		n++;
+		i++;
 	}
 	set_bool(&philos->table->info, &philos->table->done, true);
 	while (get_bool(&philos->table->info, &philos->table->finish) == false)
